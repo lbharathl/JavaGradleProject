77972894c90d3878cbad06d824fc2d0a4191f003
lbharathl <bharathboini3@gmail.com>
2021-11-27 17:09:55 +0530

    first commit

---
+++ src/main/java/com/bky/project1/SampleGradleApp/SampleGradleAppJarApplication.java
@@ -0,0 +1,13 @@
+package com.bky.project1.SampleGradleApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleGradleAppJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleGradleAppJarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bky/project1/SampleGradleApp/SampleGradleAppJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bky.project1.SampleGradleApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleGradleAppJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
